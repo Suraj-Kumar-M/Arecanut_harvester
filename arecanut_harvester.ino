16d3cf6d3370fe9bee0ff777ffceb52d57bbea33
Suraj Kumar M <84315003+Suraj-Kumar-M@users.noreply.github.com>
2025-07-06 20:50:59 +0530

    Update arecanut_harvester.ino

--- arecanut_harvester.ino
+++ arecanut_harvester.ino
@@ -6,19 +6,19 @@ Servo servo2;
 const int servoPin1 = A4;
 const int servoPin2 = A5;
 
-const int mc_rpwm = 3; //RPWM1
-const int mc_lpwm = 5; //LPWM1
-const int mc_ren = 6; //R_EN1
-const int mc_len = 7; //L_EN1
+const int mc_rpwm   = 3 ;  // RPWM1
+const int mc_lpwm   = 5 ;  // LPWM1
+const int mc_ren    = 6 ;  // R_EN1
+const int mc_len    = 7 ;  // L_EN1
 
-const int motor_b1 = 4; //RPWM3
+const int motor_b1  = 4 ; // RPWM3
 
-const int potPin1 = 13; // RC input pins
-const int potPin2 = 12;
-const int potPin3 = 11;
-const int potPin4 = 10;
-const int potPin5 = 9;
-const int potPin6 = 8;
+const int potPin1   = 13; // RC input pins
+const int potPin2   = 12;
+const int potPin3   = 11;
+const int potPin4   = 10;
+const int potPin5   = 9 ;
+const int potPin6   = 8 ;
 
 int motorPwm1 = 0;
 int motorPwm2 = 0;
@@ -33,27 +33,23 @@ void setup() {
   servo1.attach(servoPin1);
   servo2.attach(servoPin2);
 
-  pinMode(potPin1, INPUT);
-  pinMode(potPin2, INPUT);
-  pinMode(potPin3, INPUT);
-  pinMode(potPin4, INPUT);
-  pinMode(potPin5, INPUT);
-  pinMode(potPin6, INPUT);
-  
+  pinMode(potPin1, INPUT );
+  pinMode(potPin2, INPUT );
+  pinMode(potPin3, INPUT );
+  pinMode(potPin4, INPUT );
+  pinMode(potPin5, INPUT );
+  pinMode(potPin6, INPUT );
   pinMode(mc_rpwm, OUTPUT);
   pinMode(mc_lpwm, OUTPUT);
-  pinMode(mc_ren, OUTPUT);
-  pinMode(mc_len, OUTPUT);
-
-  pinMode(motor_b1, OUTPUT);
+  pinMode(mc_ren , OUTPUT);
+  pinMode(mc_len , OUTPUT);
+  pinMode(motor_b1,OUTPUT);
   
-  digitalWrite(mc_rpwm, LOW);
-  digitalWrite(mc_lpwm, LOW);
-  digitalWrite(mc_ren, LOW);
-  digitalWrite(mc_len, LOW);
- 
+  digitalWrite(mc_rpwm , LOW );
+  digitalWrite(mc_lpwm , LOW );
+  digitalWrite(mc_ren  , LOW );
+  digitalWrite(mc_len  , LOW );
   digitalWrite(motor_b1, HIGH);
-
 }
 
 void loop() 
@@ -66,30 +62,24 @@ void loop()
 void readPots() 
 { 
   motorPwm1 = map(pulseIn(potPin1, HIGH, 25000), 1100, 2000, 0, 255);
-  
-  
   motorPwm4 = map(pulseIn(potPin4, HIGH, 25000), 1100, 2000, 0, 255);
-  
   motorPwm3 = map(pulseIn(potPin3, HIGH, 25000), 1100, 2000, 0, 255);
-
   motorPwm6 = map(pulseIn(potPin6, HIGH, 25000), 1100, 2000, 0, 255);  
-  
   motorPwm5 = map(pulseIn(potPin5, HIGH, 25000), 1100, 2000, 0, 255);
   motorPwm2 = map(pulseIn(potPin2, HIGH, 25000), 1100, 2000, 0, 255);
+  
   Serial.println(motorPwm1);
   Serial.println(motorPwm2);
   Serial.println(motorPwm3);
   Serial.println(motorPwm4);
   Serial.println(motorPwm5);
   Serial.println(motorPwm6);
-  Serial.println("###");  
-  //delay(15);
+  Serial.println("###"    );  
 }
 
 void controlMotors() 
 { 
-  //Climbing motor
-  
+  // Climbing motor
   if (motorPwm6 > 0) 
   {
     if (motorPwm6 > 150) {
@@ -100,14 +90,11 @@ void controlMotors()
       stop(mc_rpwm,mc_lpwm,mc_ren,mc_len);
     }
   }
-
-  //Rotating saw blade motor
-  
+  // Rotating saw blade motor
   if (motorPwm3 > 0) 
   {
     digitalWrite(motor_b1, motorPwm3 > 150 ? LOW : HIGH);
   }
-  
 }
 
 void controlServos() 
@@ -120,31 +107,22 @@ void moveServo(Servo servo, int pin, int minAngle, int maxAngle)
 {
   int angle = map(pulseIn(pin, HIGH, 25000), 1000, 2000, minAngle, maxAngle);
   servo.write(angle);
-  
 }
 
 void up(int r_pwm,int l_pwm,int r_en,int l_en) 
 {
-  digitalWrite(r_en, HIGH);
-  digitalWrite(l_en, HIGH);  
-  digitalWrite(l_pwm,LOW);
-//  for(int i=0;i<255;i++)
-//{
-  digitalWrite(r_pwm,HIGH);
-//  delay(15);
-//}    
+  digitalWrite(r_en , HIGH);
+  digitalWrite(l_en , HIGH);  
+  digitalWrite(l_pwm, LOW );
+  digitalWrite(r_pwm, HIGH);   
 }
 
 void down(int r_pwm,int l_pwm,int r_en,int l_en) 
 {
-  digitalWrite(r_en, HIGH);
-  digitalWrite(l_en, HIGH);  
-  digitalWrite(r_pwm,LOW);
-  //for(int i=0;i<255;i++)
-  //{
-  digitalWrite(l_pwm,HIGH);
-  //delay(15);
-  //}
+  digitalWrite(r_en , HIGH);
+  digitalWrite(l_en , HIGH);  
+  digitalWrite(r_pwm, LOW );
+  digitalWrite(l_pwm, HIGH);
 }  
 
 void stop(int r_pwm,int l_pwm,int r_en,int l_en) 
